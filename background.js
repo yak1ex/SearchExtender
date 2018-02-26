e39f9bf241c7358d020bf32b9f0f6f2afcbfd338
Yak! <yak1ex@users.noreply.github.com>
2018-02-26 20:19:09 +0900

    Add working configuration.

--- background.js
+++ background.js
@@ -1,114 +1,135 @@
-// TODO: configuration
+(function () {
+  'use strict'
 
-const defConf = {
-  searches: [
-    ["Google", 5, "g", "http://www.google.co.jp/search?q=%t"],
-    ["Wikipedia(JP)", 5, "wj", "https://ja.wikipedia.org/wiki/%t"],
-    ["Wikipedia(EN)", 5, "we", "https://en.wikipedia.org/wiki/%t"]
-  ]
-}
+  /* global chrome */
+  // TODO: configuration
 
-chrome.storage.sync.set(defConf)
-
-// TODO: possible extension: restricted to specific URL
-const IDX_NAME = 0, IDX_TARGET = 1, IDX_KEY = 2, IDX_URL = 3, IDX_ISPOST = 4
-const TARGET_OMNIBOX = 1, TARGET_PAGE = 2, TARGET_SELECTION = 4, TARGET_LINK = 8, TARGET_IMAGE = 16
-const ARG_ANY = '%*', ARG_SELTEXT = '%t', ARG_URL = '%u', ARG_CLIP = '%c', ARG_LINK = '%l'
-
-let conf = [], confIdxFromName = {}, confIdxFromKey = {}
-
-function makeURL (spec, text, url)
-{
-  if (text === undefined) text = ''
-  if (url === undefined) url = ''
-  return spec.replace(ARG_SELTEXT, text).replace(ARG_URL, url).replace(ARG_ANY, text || url)
-}
-
-const defSuggest = { description: "Type search key and keyword, like 'g googling'"}
-chrome.omnibox.setDefaultSuggestion(defSuggest)
-
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  const wsPos = text.search(/\s/)
-  const key = text.slice(0, wsPos !== -1 ? wsPos : text.length)
-  let suggests = []
-  for (let c of conf) {
-    if (c[IDX_KEY].indexOf(key) !== -1) {
-      suggests.push({ content: c[IDX_KEY], description: `<match>${c[IDX_KEY]}</match> <dim>${c[IDX_NAME]}</dim>` })
-    }
+  const defConf = {
+    searches: [
+      ['Google', 5, 'g', 'http://www.google.co.jp/search?q=%t'],
+      ['Wikipedia(JP)', 5, 'wj', 'https://ja.wikipedia.org/wiki/%t'],
+      ['Wikipedia(EN)', 5, 'we', 'https://en.wikipedia.org/wiki/%t']
+    ]
   }
-  if (suggests.length === 1) {
-    chrome.omnibox.setDefaultSuggestion({ description: suggests[0].description })
-  } else {
+  chrome.storage.sync.get({ searches: [], init: false }, (res) => {
+    if (!res.init) {
+      chrome.storage.sync.set(defConf)
+    }
+
+    // TODO: possible extension: restricted to specific URL
+    const IDX_NAME = 0
+    // const IDX_TARGET = 1
+    const IDX_KEY = 2
+    const IDX_URL = 3
+    // const IDX_ISPOST = 4
+    // const TARGET_OMNIBOX = 1
+    // const TARGET_PAGE = 2
+    // const TARGET_SELECTION = 4
+    // const TARGET_LINK = 8
+    // const TARGET_IMAGE = 16
+    const ARG_ANY = '%*'
+    const ARG_SELTEXT = '%t'
+    const ARG_URL = '%u'
+    // const ARG_CLIP = '%c'
+    // const ARG_LINK = '%l'
+
+    let conf = []
+    let confIdxFromName = {}
+    let confIdxFromKey = {}
+
+    function makeURL (spec, text, url) {
+      if (text === undefined) text = ''
+      if (url === undefined) url = ''
+      return spec.replace(ARG_SELTEXT, text).replace(ARG_URL, url).replace(ARG_ANY, text || url)
+    }
+
+    const defSuggest = { description: 'Type search key and keyword, like "g googling"' }
     chrome.omnibox.setDefaultSuggestion(defSuggest)
-    suggest(suggests)
-  }
-})
 
-chrome.omnibox.onInputEntered.addListener((text, disposition) => {
-  chrome.omnibox.setDefaultSuggestion(defSuggest)
-  const wsPos = text.search(/\s/)
-  const key = text.slice(0, wsPos)
-  const query = text.slice(wsPos).trim()
-  const url = makeURL(conf[confIdxFromKey[key]][IDX_URL], query)
-  switch ( disposition ) {
-    case "currentTab":
-      chrome.tabs.update({ url })
-      break
-    case "newForegroundTab":
-      chrome.tabs.create({ url })
-      break
-    case "newBackgroundTab":
-      chrome.tabs.create({ url, active: false })
-      break
-  }
-})
-
-function setupMenu ()
-{
-  chrome.storage.sync.get('searches', (newconf) => {
-    conf = newconf.searches
-    confIdxFromName = {}
-    confIdxFromKey = {}
-// TODO: appropriate contexts handling
-    chrome.contextMenus.create({title: "SearchExtender", id: "root", contexts: ["page", "selection", "link", "image"]})
-    for (let i = 0; i < conf.length; ++i) {
-      chrome.contextMenus.create({
-        title: conf[i][IDX_NAME],
-        id: conf[i][IDX_NAME],
-        contexts: ["page", "selection", "link", "image"],
-        parentId: "root"
-      })
-      confIdxFromName[conf[i][IDX_NAME]] = i
-      confIdxFromKey[conf[i][IDX_KEY]] = i
-    }
-    chrome.contextMenus.create({
-      title: '設定',
-      id: '設定',
-      contexts: ["page", "selection", "link", "image"],
-      parentId: "root"
+    // TODO: conf by omnibox
+    chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+      const wsPos = text.search(/\s/)
+      const key = text.slice(0, wsPos !== -1 ? wsPos : text.length)
+      let suggests = []
+      for (let c of conf) {
+        if (c[IDX_KEY].indexOf(key) !== -1) {
+          suggests.push({ content: c[IDX_KEY], description: `<match>${c[IDX_KEY]}</match> <dim>${c[IDX_NAME]}</dim>` })
+        }
+      }
+      if (suggests.length === 1) {
+        chrome.omnibox.setDefaultSuggestion({ description: suggests[0].description })
+      } else {
+        chrome.omnibox.setDefaultSuggestion(defSuggest)
+        suggest(suggests)
+      }
     })
-  })
-}
 
-// TODO: open in current tab, next tab
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  console.log(info)
-  if (info.menuItemId === '設定') {
-    if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage();
-    } else {
-      window.open(chrome.runtime.getURL('options.html'));
+    chrome.omnibox.onInputEntered.addListener((text, disposition) => {
+      chrome.omnibox.setDefaultSuggestion(defSuggest)
+      const wsPos = text.search(/\s/)
+      const key = text.slice(0, wsPos)
+      const query = text.slice(wsPos).trim()
+      const url = makeURL(conf[confIdxFromKey[key]][IDX_URL], query)
+      switch (disposition) {
+        case 'currentTab':
+          chrome.tabs.update({ url })
+          break
+        case 'newForegroundTab':
+          chrome.tabs.create({ url })
+          break
+        case 'newBackgroundTab':
+          chrome.tabs.create({ url, active: false })
+          break
+      }
+    })
+
+    function setupMenu () {
+      chrome.storage.sync.get('searches', (newconf) => {
+        conf = newconf.searches
+        confIdxFromName = {}
+        confIdxFromKey = {}
+    // TODO: appropriate contexts handling
+        chrome.contextMenus.create({title: 'SearchExtender', id: 'root', contexts: ['page', 'selection', 'link', 'image']})
+        for (let i = 0; i < conf.length; ++i) {
+          chrome.contextMenus.create({
+            title: conf[i][IDX_NAME],
+            id: conf[i][IDX_NAME],
+            contexts: ['page', 'selection', 'link', 'image'],
+            parentId: 'root'
+          })
+          confIdxFromName[conf[i][IDX_NAME]] = i
+          confIdxFromKey[conf[i][IDX_KEY]] = i
+        }
+        chrome.contextMenus.create({
+          title: '設定',
+          id: '設定',
+          contexts: ['page', 'selection', 'link', 'image'],
+          parentId: 'root'
+        })
+      })
     }
-  } else {
-    const url = makeURL(conf[confIdxFromName[info.menuItemId]][IDX_URL], info.selectionText, info.linkUrl || info.srcUrl || info.pageUrl)
-    chrome.tabs.create({ url })
-  }
-})
 
-chrome.storage.onChanged.addListener((changes, area) => {
-  if(area === 'sync') {
-    if('searches' in changes) chrome.contextMenus.removeAll(() => setupMenu())
-  }
-})
+    // TODO: open in current tab, next tab
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+      console.log(info)
+      if (info.menuItemId === '設定') {
+        if (chrome.runtime.openOptionsPage) {
+          chrome.runtime.openOptionsPage()
+        } else {
+          window.open(chrome.runtime.getURL('options.html'))
+        }
+      } else {
+        const url = makeURL(conf[confIdxFromName[info.menuItemId]][IDX_URL], info.selectionText, info.linkUrl || info.srcUrl || info.pageUrl)
+        chrome.tabs.create({ url })
+      }
+    })
 
-setupMenu()
+    chrome.storage.onChanged.addListener((changes, area) => {
+      if (area === 'sync') {
+        if ('searches' in changes) chrome.contextMenus.removeAll(() => setupMenu())
+      }
+    })
+
+    setupMenu()
+  })
+})()
--- options.js
+++ options.js
@@ -1,24 +1,141 @@
-(function() {
+(function () {
   'use strict'
 
-  var restoreOptions = function () {
-    chrome.storage.sync.get('searches', function (res) {
-      for (let v of res.searches) {
-        let tr = document.createElement('tr')
-        let editButton = tr.appendChild(document.createElement('td')).appendChild(document.createElement('input'))
-        editButton.setAttribute('type', 'button')
-        editButton.setAttribute('value', 'Edit...')
-        tr.appendChild(document.createElement('td')).innerText = v[0]
-        for (let i of [1,2,4,8,16]) {
-          let cb = tr.appendChild(document.createElement('td')).appendChild(document.createElement('input'))
-          db.setAttribute('type', 'checkbox')
+  /* global chrome */
+  // TODO: import/export feature
+
+  let options
+
+  let myConfirm = (message, cb) => {
+    document.getElementById('confirmMessage').textContent = message
+    let cbTrue, cbFalse
+    let makeCb = (f) => () => {
+      document.getElementById('confirmConfirm').removeEventListener('click', cbTrue)
+      document.getElementById('cancelConfirm').removeEventListener('click', cbFalse)
+      document.getElementById('confirm').close()
+      cb(f)
+    }
+    cbTrue = makeCb(true)
+    cbFalse = makeCb(false)
+    document.getElementById('confirmConfirm').addEventListener('click', cbTrue)
+    document.getElementById('cancelConfirm').addEventListener('click', cbFalse)
+    document.getElementById('confirm').showModal()
+  }
+
+  // FIXME: sanity check
+  let showDetail = (cb) => {
+    let cbTrue, cbFalse
+    let makeCb = (f) => () => {
+      document.getElementById('update').removeEventListener('click', cbTrue)
+      document.getElementById('cancel').removeEventListener('click', cbFalse)
+      document.getElementById('detail').close()
+      cb(f)
+    }
+    cbTrue = makeCb(true)
+    cbFalse = makeCb(false)
+    document.getElementById('update').addEventListener('click', cbTrue)
+    document.getElementById('cancel').addEventListener('click', cbFalse)
+    document.getElementById('detail').showModal()
+  }
+
+  let initDetail = () => {
+    document.getElementById('update').value = 'Add'
+    document.getElementById('heading').textContent = 'New search config'
+    document.getElementById('name').value = ''
+    document.getElementById('omnibox').checked = false
+    ;['omnibox', 'page', 'selection', 'link', 'image'].forEach(id => { document.getElementById(id).checked = false })
+    document.getElementById('key').value = ''
+    document.getElementById('query').value = ''
+  }
+
+  let setDetail = v => {
+    document.getElementById('update').value = 'Update'
+    document.getElementById('heading').textContent = 'Edit search config'
+    document.getElementById('name').value = v[0]
+    ;[['omnibox', 1], ['page', 2], ['selection', 4], ['link', 8], ['image', 16]].forEach(vv => { document.getElementById(vv[0]).checked = ((v[1] & vv[1]) !== 0) })
+    document.getElementById('key').value = v[2]
+    document.getElementById('query').value = v[3]
+  }
+
+  let fromDetail = () => {
+    return [
+      document.getElementById('name').value,
+      [['omnibox', 1], ['page', 2], ['selection', 4], ['link', 8], ['image', 16]].reduce((acc, vv) => acc + (document.getElementById(vv[0]).checked ? vv[1] : 0), 0),
+      document.getElementById('key').value,
+      document.getElementById('query').value,
+      false // FIXME
+    ]
+  }
+
+  let createRow = v => {
+    let tr = document.createElement('tr')
+    tr.id = 'row' + v[0]
+    let editButton = tr.appendChild(document.createElement('td')).appendChild(document.createElement('input'))
+    editButton.setAttribute('type', 'button')
+    editButton.setAttribute('value', 'Edit...')
+    editButton.addEventListener('click', () => {
+      setDetail(v)
+      showDetail(f => {
+        if (f) {
+          let newV = fromDetail()
+          options.searches.splice(options.searches.findIndex(vv => vv[0] === v[0]), 1, newV)
+          document.getElementById('table').replaceChild(createRow(newV), tr)
         }
-        tr.appendChild(document.createElement('td')).innerText = v[2]
-        tr.appendChild(document.createElement('td')).innerText = v[3]
-        document.getElementById('table').appendChild(tr)
+      })
+    })
+    tr.appendChild(document.createElement('td')).innerText = v[0]
+    for (let j of [1, 2, 4, 8, 16]) {
+      let cb = tr.appendChild(document.createElement('td')).appendChild(document.createElement('input'))
+      cb.setAttribute('type', 'checkbox')
+      if (v[1] & j) cb.checked = true
+      cb.disabled = true
+    }
+    tr.appendChild(document.createElement('td')).innerText = v[2]
+    tr.appendChild(document.createElement('td')).innerText = v[3]
+    let delButton = tr.appendChild(document.createElement('td')).appendChild(document.createElement('input'))
+    delButton.setAttribute('type', 'button')
+    delButton.setAttribute('value', 'Del...')
+    delButton.addEventListener('click', () => myConfirm(`Delete ${v[0]}?`, (f) => {
+      if (f) {
+        options.searches.splice(options.searches.findIndex(vv => vv[0] === v[0]), 1)
+        document.getElementById('row' + v[0]).remove()
+      }
+    }))
+    return tr
+  }
+
+  let init = () => {
+    document.getElementById('new').addEventListener('click', () => {
+      initDetail()
+      showDetail((f) => {
+        if (f) {
+          let v = fromDetail()
+          options.searches.push(v)
+          document.getElementById('table').appendChild(createRow(v))
+        }
+      })
+    })
+  }
+
+  let restoreOptions = () => {
+    chrome.storage.sync.get('searches', function (res) {
+      options = res
+      options.init = true
+      for (let i = 0; i < options.searches.length; ++i) {
+        let v = options.searches[i]
+        document.getElementById('table').appendChild(createRow(v))
       }
     })
   }
 
+  let saveOptions = () => {
+    chrome.storage.sync.set(options, () => {
+      document.getElementById('status').textContent = chrome.runtime.lastError ? chrome.runtime.lastError.message : 'saved'
+      setTimeout(() => { document.getElementById('status').textContent = '' }, 750)
+    })
+  }
+
+  document.addEventListener('DOMContentLoaded', init)
   document.addEventListener('DOMContentLoaded', restoreOptions)
+  document.getElementById('save').addEventListener('click', saveOptions)
 })()
