b993616883749a5a0e3ef26b1c36734f6a6b0153
Yak! <yak1ex@users.noreply.github.com>
2018-08-07 21:07:42 +0900

    Update gulp matters.

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,21 @@ const gulp = require('gulp')
 const rename = require('gulp-rename')
 const pug = require('gulp-pug')
 
-gulp.task('default', ['firefox', 'chrome'])
-
 function process (type) {
   if (type !== 'firefox' && type !== 'chrome') return
   gulp.src('*.html').pipe(gulp.dest(type))
   gulp.src(['*.js', '!gulpfile.js']).pipe(gulp.dest(type))
   gulp.src('*.pug').pipe(pug({ locals: { [type]: true } })).pipe(gulp.dest(type))
   if (type === 'firefox') gulp.src('node_modules/dialog-polyfill/dialog-polyfill.*').pipe(rename({ dirname: '' })).pipe(gulp.dest(type))
-  gulp.src(`manifest.${type}.json`).pipe(rename('manifest.json')).pipe(gulp.dest(type))
+  return gulp.src(`manifest.${type}.json`).pipe(rename('manifest.json')).pipe(gulp.dest(type))
 }
 
 gulp.task('firefox', function () {
-  process('firefox')
+  return process('firefox')
 })
 
 gulp.task('chrome', function () {
-  process('chrome')
+  return process('chrome')
 })
+
+gulp.task('default', gulp.parallel('firefox', 'chrome'))
